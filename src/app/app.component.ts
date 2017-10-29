447effb066b2d1bc6878fc1d9d9779511de70b91
Arjun Subramanyam Varalakshmi <arjunsv3619@gmail.com>
2017-10-29 15:11:20 -0500

    Angular app deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    original: number;
+    guess: number;
+    deviation: number;
+    noOfTries: number;
+
+    constructor()
+    {
+        this.initializeGame();
+    }
+
+    initializeGame()
+    {
+        this.original = Math.floor((Math.random() * 1000) + 1);
+        this.guess = null;
+        this.deviation = null;
+        this.noOfTries = 0;
+    }
+
+    verifyGuess()
+    {
+        this.deviation = this.original - this.guess;
+        this.noOfTries = this.noOfTries + 1;
+    }
 }
